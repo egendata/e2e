a803331426cc95e4d21170da06ba96afb34cf978
Einar Persson <me@einarpersson.com>
2019-02-13 14:54:07 +0100

    Add cypress and first e2e test

---
+++ cypress/integration/spec.js
@@ -0,0 +1,25 @@
+const v4Regexp = /[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}/i
+
+describe('Consent request for example/cv', () => {
+  it('Loads auth page and displays consent request id', () => {
+    cy
+      .visit('/')
+
+    cy
+      .get('button')
+      .contains('Log in')
+      .click()
+
+    cy
+      .url()
+      .should('include', '/auth')
+
+    cy
+      .get('[data-cy="consent-request-id"]')
+      .should(res => {
+        expect(res[0].innerHTML).to.match(v4Regexp)
+      })
+
+    cy.visit('/')
+  })
+})
