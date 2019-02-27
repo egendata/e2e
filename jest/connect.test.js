725cb8d8121c4c121a6ba1b06f1a748d854c4350
Einar Persson <me@einarpersson.com>
2019-02-27 16:49:28 +0100

    Add alternative jest e2e setup, using @mydata/client as testing interface

---
+++ jest/connect.test.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const { createClient } = require('./helpers/index')
+
+describe('One client', () => {
+  let server, client
+
+  beforeEach(done => {
+    // TODO: Tell Operator to reset db
+
+    const app = express()
+    server = app.listen(0, () => {
+      // Create client with the port that the current test server is using
+      client = createClient(server.address().port)
+
+      // Hook up routes
+      app.use(client.routes)
+
+      // Setup done
+      done()
+    })
+  })
+
+  afterEach(done => {
+    server.close(done)
+  })
+
+  it('Can connect to operator', async () => {
+    await client.connect()
+
+    // Should not throw
+    expect(true)
+  })
+})
