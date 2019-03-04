64f86247f9a10bb8fb5e44f9d321426cae20c189
Alexander Czigler <alexander@czigler.eu>
2019-03-04 12:14:20 +0100

    Linux-enabler for e2e test run

---
+++ jest/helpers/ip.js
@@ -0,0 +1,18 @@
+const os = require('os')
+
+function checkOs() {
+  return os.platform()
+}
+
+module.exports = {
+  getDockerInterfaceAddress: () => {
+    switch (checkOs()) {
+      case 'linux':
+        const interfaces = os.networkInterfaces()
+        const interfaceName = Object.keys(interfaces).find(i => i.startsWith('br-'))
+        return interfaces[interfaceName].find(o => o.family === 'IPv4').address
+      default:
+        return 'host.docker.internal'
+    }
+  }
+}
