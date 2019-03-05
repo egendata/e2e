8740ac9f74bc9127a7a51d4ac683581a188ea921
Johan Öbrink <johan.obrink@gmail.com>
2019-03-05 15:55:07 +0100

    Cleanup

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-# #!/bin/bash
+# #!/bin/sh
 echo '**** Running script for e2e & integration tests ****'
 
 # TODO: Lint, Test
--- wait-for.sh
+++ wait-for.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 echo $0
 echo $1
-echo -n "Waiting for $1 to get ready..."
+printf "Waiting for $1 to get ready..."
 
 while true; do
   STATUS_CODE=`curl -sL -w "%{http_code}" -I "$1" -o /dev/null`
-  echo -n .
+  printf .
   if [ "$STATUS_CODE" = "200" ]; then
     echo
     exit 0
